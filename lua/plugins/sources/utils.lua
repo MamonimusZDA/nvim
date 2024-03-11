788e18f4faf40f7e07ba20182eb6b53e36af4df9
MamonimusZDA <jms_alejandro@hotmail.com>
2024-03-11 08:58:47 -0600

    Adding templates support

--- lua/plugins/sources/utils.lua
+++ lua/plugins/sources/utils.lua
@@ -23,9 +23,10 @@ return {
     config = true,
     keys = {
       {
-        '<leader>u', function()
+        '<leader>u',
+        function()
           require('undotree').toggle()
-        end
+        end,
       },
     },
   },
@@ -33,31 +34,57 @@ return {
     'backdround/improved-search.nvim',
     keys = {
       {
-        'n', function()  require('improved-search').stable_next() end,
-        mode = { 'n', 'x', 'o' }
+        'n',
+        function()
+          require('improved-search').stable_next()
+        end,
+        mode = { 'n', 'x', 'o' },
       },
       {
-        'N', function()  require('improved-search').stable_previous() end,
-        mode = { 'n', 'x', 'o' }
+        'N',
+        function()
+          require('improved-search').stable_previous()
+        end,
+        mode = { 'n', 'x', 'o' },
       },
       {
-        '!', function()  require('improved-search').current_word() end
+        '!',
+        function()
+          require('improved-search').current_word()
+        end,
       },
       {
-        '!', function()  require('improved-search').in_place() end,
-        mode = { 'x' }
+        '!',
+        function()
+          require('improved-search').in_place()
+        end,
+        mode = { 'x' },
       },
       {
-        '*', function()  require('improved-search').forward() end,
-        mode = { 'x' }
+        '*',
+        function()
+          require('improved-search').forward()
+        end,
+        mode = { 'x' },
       },
       {
-        '#', function()  require('improved-search').backward() end,
-        mode = { 'x' }
+        '#',
+        function()
+          require('improved-search').backward()
+        end,
+        mode = { 'x' },
       },
       {
-        '|', function()  require('improved-search').in_place() end
+        '|',
+        function()
+          require('improved-search').in_place()
+        end,
       },
     },
+  },
+  {
+    'otavioschwanck/new-file-template.nvim',
+    -- https://github.com/cvigilv/esqueleto.nvim
+    opts = {},
   },
 }

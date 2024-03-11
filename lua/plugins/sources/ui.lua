6d01eb549933c181d7ac425f2196edacaa5828d9
MamonimusZDA <jms_alejandro@hotmail.com>
2024-03-11 08:58:16 -0600

    Formatting

--- lua/plugins/sources/ui.lua
+++ lua/plugins/sources/ui.lua
@@ -13,7 +13,7 @@ return {
     commit = '9637670896b68805430e2f72cf5d16be5b97a22a',
     event = {
       'BufReadPre',
-      'BufNewFile'
+      'BufNewFile',
     },
     main = 'plugins.settings.indent-blankline-nvim',
     config = true,
@@ -28,10 +28,10 @@ return {
     commit = 'e1b72c498f25c1fc37a7e9913332c137f753a90a',
     opts = {
       anchor = {
-        up     = { width  = 0, x = -1, y =  0 },
-        bottom = { width  = 0, x =  0, y =  0 },
-        left   = { height = 0, x = -1, y = -1 },
-        right  = { height = 0, x = -1, y =  0 },
+        up = { width = 0, x = -1, y = 0 },
+        bottom = { width = 0, x = 0, y = 0 },
+        left = { height = 0, x = -1, y = -1 },
+        right = { height = 0, x = -1, y = 0 },
       },
     },
     event = 'WinNew',
@@ -41,14 +41,14 @@ return {
     'NvChad/nvim-colorizer.lua',
     event = {
       'BufReadPost',
-      'BufNewFile'
+      'BufNewFile',
     },
     main = 'plugins.settings.nvim-colorizer-lua',
     config = true,
     keys = {
       {
         '<C-c>',
-        '<Cmd>ColorizerToggle<Cr>'
+        '<Cmd>ColorizerToggle<Cr>',
       },
     },
   },
@@ -59,7 +59,7 @@ return {
       'CursorMoved',
       'CursorHold',
       'InsertEnter',
-      'CmdlineEnter'
+      'CmdlineEnter',
     },
     main = 'plugins.settings.nvim-notify',
     config = true,
@@ -69,15 +69,15 @@ return {
         function()
           require('notify').dismiss({
             silent = true,
-            pending = true
+            pending = true,
           })
-        end
+        end,
       },
       {
         '<leader>tn',
         function()
           require('telescope').extensions.notify.notify()
-        end
+        end,
       },
     },
   },
